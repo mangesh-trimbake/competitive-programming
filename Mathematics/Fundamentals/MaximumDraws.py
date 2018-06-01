ab65074c6cbdcb3546abb0881d6769b20cd7c552
Mangesh <mangesh.edu@gmail.com>
2018-06-01 22:31:49 +0530

    MaximumDraws problem modified to efficient way

--- Mathematics/Fundamentals/MaximumDraws.py
+++ Mathematics/Fundamentals/MaximumDraws.py
@@ -1,2 +1,27 @@
-for i in range(int(input())):
-    print((int(input()))+1)
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the maximumDraws function below.
+#
+def maximumDraws(n):
+    #
+    # Write your code here.
+    #
+    return n + 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = maximumDraws(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
