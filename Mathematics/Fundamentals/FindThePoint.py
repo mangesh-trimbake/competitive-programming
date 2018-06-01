7d8269b918d6ac241140497ee0855e3386014a04
Mangesh <mangesh.edu@gmail.com>
2018-06-01 22:27:26 +0530

    FindThePoint problem modified to efficient way

--- Mathematics/Fundamentals/FindThePoint.py
+++ Mathematics/Fundamentals/FindThePoint.py
@@ -1,3 +1,36 @@
-for i in range(int(input())):
-    l = list(map(int, input().strip().split(' ')))
-    print(((l[2]-l[0])+l[2]),((l[3]-l[1])+l[3]))
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the findPoint function below.
+#
+def findPoint(px, py, qx, qy):
+    #
+    # Write your code here.
+    #
+    return qx+(qx-px),qy+(qy-py)
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    for n_itr in range(n):
+        pxPyQxQy = input().split()
+
+        px = int(pxPyQxQy[0])
+
+        py = int(pxPyQxQy[1])
+
+        qx = int(pxPyQxQy[2])
+
+        qy = int(pxPyQxQy[3])
+
+        result = findPoint(px, py, qx, qy)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
