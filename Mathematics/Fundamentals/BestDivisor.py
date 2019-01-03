92ab6c36d5987ca4dc700e1e5700bfc8475462ec
mangesh-trimbake <mangeshtrimbake00786.94@gmail.com>
2019-01-04 00:32:23 +0530

    Best Divisor issue - two best divisor which has same sum,then smaller one should take

--- Mathematics/Fundamentals/BestDivisor.py
+++ Mathematics/Fundamentals/BestDivisor.py
@@ -12,6 +12,12 @@ def getSumOfDigit(n):
     # print (n,sum(nl))
     return sum(nl)
 
+def smaller(i,j):
+    if(i<j):
+        return i
+    else:
+        return j
+
 def divisors(n):
     divs = [1]
     large = 1
@@ -19,16 +25,26 @@ def divisors(n):
         if n%i == 0:
             divs.extend([i,n/i])
             # print(i,getSumOfDigit(i))
-            if(getSumOfDigit(i)>getSumOfDigit(large)):
-                large = i
+            if(getSumOfDigit(i)>=getSumOfDigit(large)):
+                if(getSumOfDigit(i)==getSumOfDigit(large)):
+                    large = smaller(i,large)
+                else:
+                    large = i
                 # print(large)
             # print(n/i,getSumOfDigit(n/i))
-            if(getSumOfDigit(n/i)>getSumOfDigit(large)):
-                large = n/i
+            if(getSumOfDigit(n/i)>=getSumOfDigit(large)):
+                if(getSumOfDigit(n/i)==getSumOfDigit(large)):
+                    large = smaller(n/i,large)
+                else:
+                    large = n/i
                 # print(large)
     divs.extend([n])
-    if(getSumOfDigit(n)>getSumOfDigit(large)):
-        large = n
+    # print(n,getSumOfDigit(large))
+    if(getSumOfDigit(n)>=getSumOfDigit(large)):
+        if(getSumOfDigit(n)==getSumOfDigit(large)):
+            large = smaller(n,large)
+        else:
+            large = n
         # print(large)
     
     return list(set(divs)),large
