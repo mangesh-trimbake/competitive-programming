f5efbdfde6cb56ccf86c9f614681206aa19686a4
mangesh-trimbake <mangeshtrimbake00786.94@gmail.com>
2019-01-11 23:44:16 +0530

    Special Multiple

---
+++ Mathematics/Fundamentals/SpecialMultiple.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import os
+import sys
+
+# Complete the solve function below.
+def solve(n):
+    i = 1
+    while True:
+        j = int(bin(i)[2:].replace('1','9'))
+        if j % n == 0:
+            break
+        i+=1
+    print(j)
+    return str(j)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = solve(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
+
