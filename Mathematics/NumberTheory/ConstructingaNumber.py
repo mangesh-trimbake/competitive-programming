c9856ed4de07da0a6c103456118b4b03e088bcc3
mangesh-trimbake <mangeshtrimbake00786.94@gmail.com>
2019-02-20 18:38:02 +0530

    Constructing a Number

---
+++ Mathematics/NumberTheory/ConstructingaNumber.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the canConstruct function below.
+def canConstruct(a):
+    # Return "Yes" or "No" denoting whether you can construct the required number.
+    sum = 0
+    for i in a:
+        # print(i)
+        for st in str(i):
+            sum =sum + int(st)
+    
+    if(sum%3==0):
+        return "Yes"
+    else:
+        return "No"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = canConstruct(a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
+
