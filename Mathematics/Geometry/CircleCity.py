b6efe70c1da6e473105d960c1430d4cac5aa0567
mangesh-trimbake <mangeshtrimbake00786.94@gmail.com>
2019-03-28 23:22:56 +0530

    Circle City modified

--- Mathematics/Geometry/CircleCity.py
+++ Mathematics/Geometry/CircleCity.py
@@ -1,54 +1,21 @@
-#!/bin/python
+from math import sqrt, ceil
+from sys import stdin
 
-from __future__ import print_function
 
-import os
-import sys
+def main():
+    def ans(y2, r2, k):
+        count = 0
+        for x in xrange(int(ceil(sqrt(r2)))):
+            if (r2 - x*x) in y2:
+                count += 4
+                if count > k:
+                    return 'impossible'
+        return 'possible'
 
-# Complete the solve function below.
-def solve(d, k):
-    
-    r2, k = d,k
+    inp = stdin.readlines()
+    y2 = set(x*x for x in xrange(int(ceil(sqrt(2000000000)))))
+    for _ in xrange(int(inp[0])):
+        print ans(y2, *map(int, inp[_ + 1].split()))
 
-    required = 0
-    r = r2 ** 0.5
 
-    for x in xrange(0, int(r) + 1):
-        low = 0
-        high = int(r) + 1
-        while low <= high:
-            mid = (low + high) / 2
-            if x * x + mid * mid == r2:
-                if mid == 0 or x == 0:
-                    required += 2
-                else:
-                    required += 4
-                if required > k: return "impossible"
-                break
-            elif x * x + mid * mid < r2:
-                low = mid + 1
-            else:
-                high = mid - 1
-
-    if required > k: return "impossible"
-
-    return "possible"
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    t = int(raw_input())
-
-    for t_itr in xrange(t):
-        dk = raw_input().split()
-
-        d = int(dk[0])
-
-        k = int(dk[1])
-
-        result = solve(d, k)
-
-        fptr.write(result + '\n')
-
-    fptr.close()
-
+main()
