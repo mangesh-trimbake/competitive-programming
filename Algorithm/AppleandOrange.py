8184ade94cf069b47056b8bd7d312432c081b6df
Mangesh <mangesh.edu@gmail.com>
2019-04-15 22:17:11 +0530

    Apple and Orange

---
+++ Algorithm/AppleandOrange.py
@@ -0,0 +1,38 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    print(sum([1 for each in apples if s <= (each + a) <=t]))
+    print(sum([1 for each in oranges if s <= (each + b) <=t]))
+
+if __name__ == '__main__':
+    st = raw_input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = raw_input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = raw_input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = map(int, raw_input().rstrip().split())
+
+    oranges = map(int, raw_input().rstrip().split())
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
+
